a087f0c04bb942b0d4c2444fe367fa379ed81283
Ryan H. Code <42245712+ryanhcode@users.noreply.github.com>
2022-09-03 19:40:09 -0700

    Chomping chests

--- common/src/main/java/gg/moonflower/carpenter/common/block/CarpenterChestBlockEntityRenderer.java
+++ common/src/main/java/gg/moonflower/carpenter/common/block/CarpenterChestBlockEntityRenderer.java
@@ -18,6 +18,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.ChestType;
 
 import java.util.Map;
 
@@ -47,8 +48,25 @@ public class CarpenterChestBlockEntityRenderer implements BlockEntityRenderer<Ca
 
         final Map<ResourceLocation, BakedModel> bakedRegistry = modelManagerAccessor.getBakedRegistry();
 
-        BakedModel body = bakedRegistry.getOrDefault(chestType.body(), modelManager.getMissingModel());
-        BakedModel lid = bakedRegistry.getOrDefault(chestType.lid(), modelManager.getMissingModel());
+        ResourceLocation bodyLoc = chestType.body();
+        ResourceLocation lidLoc = chestType.lid();
+        ChestType connectionType = blockState.getValue(ChestBlock.TYPE);
+
+        switch (connectionType) {
+            case LEFT -> {
+                bodyLoc = chestType.rightBody();
+                lidLoc = chestType.rightLid();
+                break;
+            }
+            case RIGHT -> {
+                bodyLoc = chestType.leftBody();
+                lidLoc = chestType.leftLid();
+                break;
+            }
+        }
+
+        BakedModel body = bakedRegistry.getOrDefault(bodyLoc, modelManager.getMissingModel());
+        BakedModel lid = bakedRegistry.getOrDefault(lidLoc, modelManager.getMissingModel());
 
         ms.pushPose();
         ms.translate(0.5, 0.5, 0.5);
@@ -57,7 +75,10 @@ public class CarpenterChestBlockEntityRenderer implements BlockEntityRenderer<Ca
         blockRenderDispatcher.getModelRenderer().renderModel(ms.last(), buffer.getBuffer(Sheets.solidBlockSheet()), (BlockState)null, body, 1.0F, 1.0F, 1.0F, packedLight, OverlayTexture.NO_OVERLAY);
 
         ms.pushPose();
-//        ms.mulPose();
+        double funny = (Math.sin(blockEntity.getLevel().getGameTime() + partialTick) / 2 + 0.5) * 0.2;
+        ms.translate(0.0, 9 / 16.0, 14 / 16.0);
+        ms.mulPose(Vector3f.XP.rotationDegrees((float) (funny * 100.0f)));
+        ms.translate(0.0, -9 / 16.0, -14 / 16.0);
         blockRenderDispatcher.getModelRenderer().renderModel(ms.last(), buffer.getBuffer(Sheets.solidBlockSheet()), (BlockState)null, lid, 1.0F, 1.0F, 1.0F, packedLight, OverlayTexture.NO_OVERLAY);
         ms.popPose();
         ms.popPose();
